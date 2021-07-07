01de595d46e670e3c66851798e013b1eeda72d83
muzammilhayath <muzammilhayath@192.168.1.100>
2021-07-07 18:36:27 +0530

    Typecast asciimethod

---
+++ ALMAMATTER/src/TypecastACSII.java
@@ -0,0 +1,14 @@
+
+public class TypecastACSII {
+
+	public static void main(String[] args) {
+		// Finding ASCII using typecast.
+		
+		char ch='A';
+		int castAscii= (int)ch;
+		
+		System.out.println("The ascii is:"+castAscii);
+
+	}
+
+}
